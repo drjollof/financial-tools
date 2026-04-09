dea48058deccb209601e8ab191b27ee75e0d0ea2
drjollof <drjollof@gmail.com>
2026-04-09 12:45:42 +0100

    updated 3 pages...

--- pages/central_bank.py
+++ pages/central_bank.py
@@ -1,40 +1,94 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 
 
-st.header('Fractional Reserve Banking Simulator')
-st.divider()
-
-def bank_reserve(initial_deposit, RR: int):
-    reserve = (RR/100) * initial_deposit
-    lendable = initial_deposit - reserve
-    return reserve, lendable
+st.title("Fractional Reserve Banking Simulator")
+st.caption(f"*Calculations assume a closed banking system where all lent money is re-deposited.*")
 
 
 
+def get_metrics(reserve_ratio, initial_deposit, interest_margin):
 
-col1, col2 = st.columns(2,border=True)
+    multiplier = 1 / (reserve_ratio / 100)
+    total_system_deposits = initial_deposit * multiplier
+
+    total_loans = total_system_deposits * (1 - (reserve_ratio / 100))
+    annual_profit = total_loans * (interest_margin / 100)
+
+    liquidity_buffer = initial_deposit * (reserve_ratio / 100)
+
+
+    return total_system_deposits , annual_profit , multiplier , liquidity_buffer
+
+
+def get_stress_test(withdrawal_percent, total_system_deposits):
+    withdrawal_amount = total_system_deposits * (withdrawal_percent / 100)
+
+    return withdrawal_amount
+    
+
+st.subheader("Bank Parameters")
+col1, col2, col3 = st.columns(3 , border=True)
 
 with col1:
-    initial_deposit = st.number_input('**Initial Deposit($)**')
-
-    rr = st.slider('**Reserve Ratio(%)**',
-                   value= 4)
-
-    required_reserve , lendable = bank_reserve(initial_deposit, rr)
-
-
+    initial_deposit = st.number_input("Initial Deposit ($)", 
+                                      value=10000, 
+                                      )
 with col2:
-    category = ['Required Reserves', 'Lendable Funds']
-    values = [required_reserve, lendable]
-    fig,ax = plt.subplots()
+    reserve_ratio = st.slider("Reserve Ratio (%)",
+                               1, 50, 10,
+                                 help="The % of deposits the bank must keep in the vault.")
+with col3:
+    interest_margin = st.slider("Interest Margin (%)", 
+                                0.5, 10.0, 4.0, 
+                                help="The profit gap between loan rates and deposit rates.")
 
-    if len(values) == 0 or np.nansum(values) == 0:
-        st.warning("**No valid data available to display the pie chart.**")
-    else:
 
-        ax.pie(values, labels=category, autopct="%.0f%%")
-        ax.axis('equal')
+total_system_deposits , annual_profit , multiplier, liquidity_buffer = get_metrics(reserve_ratio, initial_deposit, interest_margin)
 
-        st.pyplot(fig)
+st.write("---")
+m1, m2, m3 = st.columns(3, border=True)
+
+
+m1.metric("Total System Deposits",
+           value= f"${total_system_deposits:,.0f}",
+           help=f"Money Multiplier: {multiplier:.0f}x")
+
+m2.metric("Bank Annual Profit",
+          value=  f"${annual_profit:,.0f}",
+            help="Estimated income from re-lending deposits.")
+
+m3.metric("Liquidity Buffer", 
+          value= f"${initial_deposit:,.0f}",
+           
+            help="The actual cash available for withdrawals.")
+
+
+
+st.write("---")
+st.subheader("Withdrawal Stress Test")
+
+container1 = st.container(border=True)
+
+container2 = st.container(border=True)
+col4 = st.columns(1, border=True)
+
+withdrawal_percent = container1.slider(" **Percentage Withdrawal (%)** ",
+                                    help="What % of customers want their cash back at the same time?")
+
+
+withdrawal_amount = get_stress_test(withdrawal_percent, total_system_deposits)
+
+
+
+if withdrawal_amount <= initial_deposit:
+    container2.success(f"**STABLE** \n\n **The bank has enough liquidity to cover ${withdrawal_amount:,.0f} in withdrawals** ")
+
+else:
+    container2.error("**INSOLVENT** \n\n  **Withdrawals exceed available cash** ")
+    st.divider()
+    container2.warning("**LIQUIDITY CRISIS** \n\n **You would need an emergency loan from the Central Bank** ")
+
+
+
+
+
--- pages/sharpe_analyzer.py
+++ pages/sharpe_analyzer.py
@@ -1,39 +1,84 @@
 import streamlit as st
 
-def get_sharpe(asset_return, volatility, risk_free):
+def get_metrics(asset_return, volatility, risk_free):
     if volatility > 0:
-        sharpe = (asset_return - risk_free) / volatility
+        premium = asset_return - risk_free
+        sharpe = premium / volatility
+        
     else:
+        premium = asset_return - risk_free
         sharpe = 0
-    return sharpe
+    return sharpe , premium
+
 
 st.header('Risk-Adjusted Performance (Sharpe Ratio)')
 st.divider()
 
-risk_free = st.slider('**Risk-Free Rate (%)**',
-                      max_value=10)
+container0 = st.container(border=True)
+
+rf = container0.slider('**Risk-Free Rate (%)**',
+                      max_value=10,
+                      help="The return on a safe investment, like a Treasury Bill")
 
 
 col1 , col2 = st.columns(2, border=True)
 with col1: 
-    asset_return = st.number_input('**Asset Return (%)**',
-                                   value= 4)
+    rp = st.number_input('**Expected Asset Return (%)**',
+                                   value= 4,
+                                   help="The total return you expect from the risky asset.")
 
 with col2:
-    annual_volatility = st.number_input('**Annual Volatility (%)**',
-                                        value= 5)
+    sigma = st.number_input('**Asset Volatility (%)**',
+                                        value= 5,
+                                        help="The standard deviation of returns. This represents the 'stress' or price swings of the investment.")
 
 
-sharpe_ratio = get_sharpe(asset_return, annual_volatility, risk_free)
+sharpe_ratio , premium = get_metrics(rp, sigma, rf)
+
+previous_ratio = sharpe_ratio
+
 st.divider()
+
 container = st.container(border= True, horizontal= True, 
                          horizontal_alignment="center")
 
-container.metric(
+col3, col4 = container.columns(2, border=True)
+
+with col3:
+    st.metric('**MARKET PREMIUM**',
+              value=f'{premium:.2f}%',
+              help="This is the excess return earned specifically for taking the risk."
+              )
+
+
+
+
+with col4:
+    st.metric(
         label="**SHARPE RATIO**",
         value=f'{sharpe_ratio}',
+        help="This is the investment's return relative to its risk"
         )
+    
+
+container2 = st.container(border=True)
+
+if sharpe_ratio >= 1.0 and sigma > 0:
+    container2.success(" **You are getting high returns relative to the risk**")
+
+elif sharpe_ratio > 0 and sigma > 0:
+    
+    container2.warning(" **You are being compensated for the risk, but there may be more efficient options** ")
+
+elif sharpe_ratio < 0 and sigma > 0:
+   
+    container2.error(" **Your return is lower than the risk-free rate. You are taking risk for no extra reward** ")
 
 
+elif sigma == 0:
+    container2.error(' **Volatitility is currently at 0. This indicates little to no risk on investment** ')
 
+else:
+    container2.info(" **You are not earning anything above what you could have made from a 'safe' investment** ")
 
+    
